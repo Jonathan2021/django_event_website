b5d74bd2b54df48701250554b58b231ab1bf6304
Jonathan Sands <misterjonathansands@gmail.com>
2019-03-28 11:14:59 +0100

    commented out status and adress because We probably don't need them, added Email model, added a name and translation to each field

--- billeterie_assos/event/models.py
+++ billeterie_assos/event/models.py
@@ -6,6 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
+class Email(models.Model):
+    email = models.EmailField(_("Email address"))
+
+
+"""
+class Address(models.Model):
+    zip_code = models.CharField(max_length=5, validators=[validate_zipcode]
+"""
+
+
 def validate_birth(value):
     if value > datetime.date.today():
         raise ValidationError(_('%(value) is bigger than today'),
@@ -13,10 +23,11 @@ def validate_birth(value):
 
 
 class User(models.Model):
-    email_id = models.ForeignKey(Email, on_delete = models.RESTRICT)
-    firstname = models.CharField(max_length=64)
-    lastname = models.CharField(max_length=64)
-    gender = models.BooleanField()
-    birth = models.DateField(validators=[validate_birth])
-    status = models.IntegerField()
-    adress_id = models.ForeignKey(Address, on_delete = models.SET_NULL, null=True)
+    email_id = models.ForeignKey(Email, on_delete=models.RESTRICT)
+    firstname = models.CharField(_("First name"), max_length=64)
+    lastname = models.CharField(_("Last name"), max_length=64)
+    gender = models.BooleanField(_("Gender"))
+    birth_date = models.DateField(_("Birth Date"), validators=[validate_birth])
+    # status = models.IntegerField()
+    # adress_id = models.ForeignKey(Address,
+    # on_delete = models.SET_NULL, null=True)

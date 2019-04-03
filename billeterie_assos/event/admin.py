1127d02d9c9b105124c0a45a6a1612accc8093b7
Jonathan Sands <misterjonathansands@gmail.com>
2019-04-03 10:39:34 +0200

    cant manage to make a radio to select as manager and/or president in member inline

--- billeterie_assos/event/admin.py
+++ billeterie_assos/event/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Profile, Association, Member
+from .forms import MemberAdminFormset
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name = _("Extra")
+    verbose_name_plural = _("Extras")
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+
+class MemberInline(admin.TabularInline):
+    model = Member
+    formset = MemberAdminFormset
+
+
+class AssociationAdmin(admin.ModelAdmin):
+    inlines = [MemberInline]
+    fieldsets = [
+            (_('Name of the association'), {'fields': ['name']})
+    ]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Association, AssociationAdmin)
---
+++ billeterie_assos/event/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Member, Manager, President
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+from .models import Profile
+
+
+class CustomUserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.get_full_name()
+
+
+class ProfileModelChoiceField(forms.ChoiceField):
+    def label_from_instance(self, obj):
+        return "%s (%s)" % (obj.user.get_full_name(), obj.user.username)
+
+
+class MemberAdminForm(forms.BaseInlineFormSet):
+    manager = forms.BooleanField()
+    president = forms.BooleanField()
+    profile_id = ProfileModelChoiceField
+
+    def clean(self):
+        if self.president and not self.manager:
+            raise ValidationError(_('A president is necessarily a Member \
+                of the Bureau'))
+        else:
+            super(MemberAdminForm, self).clean()
+
+    def save(self, commit=True):
+        member = super(MemberAdminForm, self).save(commit=commit)
+        if member.assos_id and member.profile_id:
+            if self.manager:
+                new_manager = Manager.objects.create(
+                        profile_id=member.profile_id,
+                        assos_id=member.assos_id
+                                                    )
+                if self.president:
+                    President.object.create(
+                            profile_id=new_manager.profile_id,
+                            assos_id=new_manager.assos_id
+                                           )
+            elif self.president:
+                raise ValidationError(_('A president is necessarily a Member \
+                        of the Bureau (how did it pass first test?)'))
+        return member
+
+    class Meta:
+        model = Member
+        fields = ['assos_id', 'profile_id', 'manager', 'president']
+
+
+MemberAdminFormset = forms.formset_factory(Member, MemberAdminForm)

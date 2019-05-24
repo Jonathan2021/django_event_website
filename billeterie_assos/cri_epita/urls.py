b5c6c049f114b7082ac4bf3d42696f22041ef902
Clement Davin <clement.davin@epita.fr>
2019-05-25 00:32:43 +0200

    still same errors but changed stuff

--- billeterie_assos/cri_epita/urls.py
+++ billeterie_assos/cri_epita/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 from django.contrib.auth import logout
 from django.conf.urls import include
@@ -7,8 +9,8 @@ app_name = 'cri'
 
 urlpatterns = [
         url(r'login/', views.login.as_view(), name='login'),
-        url(r'logout/', logout, {'next_page': '/'}, name="logout"),
-        url(r'log/', views.log.as_view(), name="log"),
+        url(r'logout/', logout, {'next_page': '/'}, name="logout"), 
         path('log/', TemplateView.as_view(template_name="log.html")),
-        url(r'^', include((social_django_urls,'social_django'), namespace='social'))
+        url(r'log/', views.log.as_view(), name="log"),
+        url(r'^', include((social_django_urls,'cri_epita'), namespace='social'))
         ]

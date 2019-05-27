1de4b4397c16e56f0ff57253aecfe27bcbd706d5
Clement Davin <clement.davin@epita.fr>
2019-05-27 18:45:18 +0200

    ERROR FIXED git add billeterie_assos/urls.py cri_epita/urls.py  but have to fix html page

--- billeterie_assos/billeterie_assos/urls.py
+++ billeterie_assos/billeterie_assos/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('event.urls')),
+    url(r'', include('social_django.urls' ,namespace='social'))
 ]
--- billeterie_assos/cri_epita/urls.py
+++ billeterie_assos/cri_epita/urls.py
@@ -10,7 +10,6 @@ app_name = 'cri'
 urlpatterns = [
         url(r'login/', views.login.as_view(), name='login'),
         url(r'logout/', logout, {'next_page': '/'}, name="logout"), 
-        path('log/', TemplateView.as_view(template_name="log.html")),
         url(r'log/', views.log.as_view(), name="log"),
-        url(r'^', include((social_django_urls,'cri_epita'), namespace='social'))
+        url(r'', include('social_django.urls' ,namespace='social'))
         ]

953264d80b10402371cc530c922ea3d824f52fc9
Clement Davin <clement.davin@epita.fr>
2019-05-30 23:23:52 +0200

    good shop and pretty good printing

--- billeterie_assos/shop/urls.py
+++ billeterie_assos/shop/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.index_shop, name='home'),
-    path('shop/', views.index_shop, name='index_shop'),
+    path('shop', views.index_shop, name='index_shop'),
     path('product/<int:product_id>/<slug:product_slug>/',
         views.show_product, name='product_detail'),
     path('cart/', views.show_cart, name='show_cart'),

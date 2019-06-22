41fd2e9290fd2a8eb495f113504ef3898916c615
Jonathan Sands <jonathan.sands@epita.fr>
2019-06-22 14:31:04 +0200

    linked signals etc. correctly

--- billeterie_assos/event/apps.py
+++ billeterie_assos/event/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class EventConfig(AppConfig):
     name = 'event'
+    verbose_name = _('event')
 
     def ready(self):
-        import users.signals
+        import event.signals  # noqa

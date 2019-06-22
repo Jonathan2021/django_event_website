779d59aecdac840a80d669ca7d584955e25edf97
Jonathan Sands <jonathan.sands@epita.fr>
2019-06-22 14:10:04 +0200

    seemed like I already had signal in event/signals.py so I cleaned the whole thing up

--- billeterie_assos/event/models.py
+++ billeterie_assos/event/models.py
@@ -41,15 +41,6 @@ class Profile(models.Model):
         verbose_name = _("Profile")
         verbose_name_plural = _("Profiles")
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 
 class EmailAddress(models.Model):
     email = models.EmailField(_("Email address"), unique=True)

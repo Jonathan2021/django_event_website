93d647b841483893a1e0200dd2165651faf2dad1
Jonathan Sands <jonathan.sands@epita.fr>
2019-06-24 13:58:41 +0200

    on creation, event sends an email to president

--- billeterie_assos/event/signals.py
+++ billeterie_assos/event/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from event.models import Profile
+from event.models import Profile, Event, Boss
+from django.core.mail import send_mail
+from django.urls import reverse
 
 
 @receiver(post_save, sender=User)
@@ -13,3 +15,15 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(post_save, sender=Event)
+def send_email_event(sender, instance, created, **kwargs):
+    if created:
+        send_mail(
+            'Validate an event',
+            'This event needs validation.\n%s' % (reverse(
+                    'event:event_detail', kwargs={'pk': instance.pk})),
+            'billeterie@epita.com',
+            [instance.assos_id.president.user.email],
+            fail_silently=False,)

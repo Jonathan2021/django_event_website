3aa0a93ee3ffaf6938ad0bbc1e733b9c84e2216b
Jonathan Sands <jonathan.sands@epita.fr>
2019-06-24 15:25:11 +0200

    fixed error in MyUserCreationForm, sign up now works as expected

--- billeterie_assos/accounts/forms.py
+++ billeterie_assos/accounts/forms.py
@@ -13,7 +13,7 @@ class MyUserCreationForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(MyUserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()

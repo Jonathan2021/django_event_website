f1a587cf3fb5e52c4518d7dd0465855128376328
Jonathan Sands <jonathan.sands@epita.fr>
2019-06-24 15:32:11 +0200

    asks for validation or approval or nothing when creating an event

--- billeterie_assos/event/signals.py
+++ billeterie_assos/event/signals.py
@@ -18,13 +18,22 @@ def save_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-@receiver(post_save, sender=Event)
+@receiver(post_save, sender=Event)  # maybe cleaner to create email object and modify it with if then send it
 def send_email_event(sender, instance, created, **kwargs):
     if created:
-        send_mail(
-            'Validate an event',
-            'This event needs validation.\n%s' % absoluteuri.build_absolute_uri(reverse(
-                    'event:event_detail', kwargs={'pk': instance.pk})),
-            'billeterie@epita.com',
-            [instance.assos_id.president.user.email],
-            fail_silently=False,)
+        if instance.event_state == Event.PENDING:
+            send_mail(
+                'Validate an event',
+                'This event needs validation.\n%s' % absoluteuri.build_absolute_uri(reverse(
+                        'event:event_detail', kwargs={'pk': instance.pk})),
+                'billeterie@epita.com',
+                [instance.assos_id.president.user.email],
+                fail_silently=False,)
+        elif instance.event_state == Event.VALIDATED:
+            send_mail(
+                'Approve an event',
+                'This event needs approval.\n%s' % absoluteuri.build_absolute_uri(reverse(
+                        'event:event_detail', kwargs={'pk': instance.pk})),
+                'billeterie@epita.com',
+                [Boss.objects.first().user.email],
+                fail_silently=False,)

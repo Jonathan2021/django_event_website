b6eb01f885c1d47fbb0bff766c05f2abe1e72e12
Clement Davin <clement.davin@epita.fr>
2019-06-26 21:42:46 +0200

    my upcomming events working !

--- billeterie_assos/event/urls.py
+++ billeterie_assos/event/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('event/', views.EventListView.as_view(), name='events'),
     path('event/<int:pk>', views.EventDetailView.as_view(), name='event_detail'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('upcomming_events/', views.Upcomming_Events, name='upcomming_events'),
     path('assos/delete/<int:pk>', views.AssosDelete.as_view(), name='assos_delete'),
     path('assos/<int:asso_pk>/delete/member/<int:pk>', views.MemberDelete.as_view(), name='delete_member'),
     path('assos/<int:asso_pk>/manager/add/<int:pk>', views.ManagerCreate.as_view(), name='add_manager'),
